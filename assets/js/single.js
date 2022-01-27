d0c6ba0bf5b9dd23d83f7fc000bdae2a150089fb
Diego Kaku <diegokaku@gmail.com>
2022-01-26 23:15:43 -0600

    lesson 6.4.6 done, final

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,27 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var queryString = document.location.search;
+var repoName = queryString.split("=")[1];
+console.log(repoName);
+
+var getRepoName = function () {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
 
 var getRepoIssues = function (repo) {
   // format the github api url
@@ -19,8 +40,8 @@ var getRepoIssues = function (repo) {
         }
       });
     } else {
-      console.log(response);
-      alert("There was a problem with your request!");
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
@@ -78,4 +99,4 @@ var displayWarning = function (repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
